fa6b4876cafcdd873fa0e51b4bd8991048a4af55
Dan Simek <djsimek@yahoo.com>
2025-08-15 23:12:27 -0500

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Dan Simek on 8/15/25.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
